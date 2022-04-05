aebfa65e88f199ec454050e483cf7b699addfa86
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-05 14:39:56 -0300

    Delete src directory

--- src/main/java/com/tuyo/clinicaapi/repository/PacienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tuyo.clinicaapi.repository;
-
-import com.tuyo.clinicaapi.model.Paciente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PacienteRepository extends JpaRepository<Paciente, Integer> {
-}
